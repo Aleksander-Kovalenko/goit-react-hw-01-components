042ce30776741b88a1ee3e3791580242cfc0e73a
Aleksander-Kovalenko <coldfellow@gmail.com>
2021-12-12 17:24:42 +0200

    распаковка React

--- build/static/js/main.0398222c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var c=n(1),o=n.n(c),s=n(3),r=n.n(s),i=(n(8),n(0));var a=function(){return Object(i.jsx)("div",{className:"App"})};r.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.0398222c.chunk.js.map
