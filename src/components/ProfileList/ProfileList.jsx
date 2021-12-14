eeedaff97a60911880d231bbaf0c202dc80fac30
Aleksander-Kovalenko <coldfellow@gmail.com>
2021-12-14 23:01:01 +0200

    добавления компонентов карточки

---
+++ src/components/ProfileList/ProfileList.jsx
@@ -0,0 +1,14 @@
+import { Profile } from '../Profile/Profile';
+
+export const ProfileList = ({ events }) => {
+  return events.map(item => (
+    <Profile
+      key={item.tag}
+      username={item.username}
+      tag={item.tag}
+      location={item.location}
+      avatar={item.avatar}
+      stats={item.stats}
+    ></Profile>
+  ));
+};
