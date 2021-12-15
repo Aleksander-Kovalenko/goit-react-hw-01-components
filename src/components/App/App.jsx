2db7661fe094f573840fb20fb07729d460e8ab4f
Aleksander-Kovalenko <coldfellow@gmail.com>
2021-12-15 10:21:57 +0200

    done task#1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,10 @@
-import { PageTitle } from '../PageTitle/PageTitle';
-import { EventBoard } from '../EventBoard/EventBoard';
-
-import upcomingEvents from '../../upcoming-events.json';
+import { ProfileList } from 'components/ProfileList/ProfileList';
+import user from '../../user.json';
 
 export const App = () => {
   return (
     <>
-      <PageTitle text="24th Core Worlds Coalition Conference" />
-      <EventBoard events={upcomingEvents} />
+      <ProfileList events={user} />
     </>
   );
 };
